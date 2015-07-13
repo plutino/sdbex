1671cd1fa4a66e8694f5fc2c70621367011eb417
Ke Zhang <ke.zhang@appfolio.com>
2015-07-12 22:03:16 -0700

    kz - initial code to show items

--- lib/view/item.rb
+++ lib/view/item.rb
@@ -16,27 +16,41 @@ module SDBMan
           padding: '5'
         )
         
+        @items = TkVariable.new_hash
         @item_tbl = Tk::TkTable.new(@frame,
-          borderwidth: 0,
-          xscrollcommand: proc {|*args| @xscrollbar.set(*args)},          
-          yscrollcommand: proc {|*args| @yscrollbar.set(*args)}          
+          #borderwidth: 1,
+          titlecols: 1,
+          titlerows: 1,
+          cache: true,
+          font: TkFont.new(size: 14),
+          colstretchmode: 'unset',
+          cursor: 'top_left_arrow',
+          drawmode: 'slow',
+          flashmode: false,
+          state: :disabled,
+          selecttype: 'row',
+          selectmode: 'extended',
+          variable: @items,
         ).grid(row: 0, column: 0, sticky: 'nwse')
-        @xscrollbar = Ttk::Scrollbar.new(@frame,
-          orient: 'horizontal',
-          command: proc {|*args| @item_tbl.xview(*args)}
-        ).grid(row: 1, column: 0, sticky: 'nwse')
-        @yscrollbar = Ttk::Scrollbar.new(@frame,
-          orient: 'vertical',
-          command: proc {|*args| @item_tbl.yview(*args)}
-        ).grid(row: 0, column: 1, sticky: 'nwse')
+        @item_tbl.xscrollbar(Ttk::Scrollbar.new(@frame).grid(row: 1, column: 0, sticky: 'nwse'))
+        @item_tbl.yscrollbar(Ttk::Scrollbar.new(@frame).grid(row: 0, column: 1, sticky: 'nwse'))
         TkGrid.columnconfigure @frame, 0, weight: 1
         TkGrid.rowconfigure @frame, 0, weight: 1
       end
       
       def reload
         d = @data.items
-        @item_tbl['cols'] = @data.attr_ct
-        @item_tbl['rows'] = d.count + 1
+        @item_tbl['cols'] = @data.attr_ct + 1
+        @item_tbl['rows'] = d.count
+
+        unless d.empty?
+          d.each_with_index do |row, ridx|
+            row.each_with_index do |v, cidx|
+              @items[ridx, cidx] = v unless v.nil?
+            end
+          end            
+        end
+        
       end
       
     end
